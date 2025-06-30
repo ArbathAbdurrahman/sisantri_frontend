c93166095bc1546f822858dae8e1f719227abecd
Arbath Abdurrahman <arbath21@gmail.com>
2025-06-30 12:17:15 +0700

    Base

---
+++ src/routes/+page.svelte
@@ -0,0 +1,4 @@
+<div class="min-h-screen flex items-center justify-center bg-red-700">
+  <h1 class="text-white text-9xl text-center font-extrabold">KERJA KERJA KERJA JANGAN MALASSS!!!</h1>
+</div>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
